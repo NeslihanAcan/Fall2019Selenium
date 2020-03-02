07d99c3dddae04dfe446f376f2326c18947e66e3
NeslihanAcan <neslihan3018@hotmail.com>
2020-03-01 22:49:03 -0500

    Google Search Test

---
+++ src/test/java/day2/Deneme.java
@@ -0,0 +1,28 @@
+package day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Deneme {
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        WebElement search = driver.findElement(By.name("q"));
+        Thread.sleep(2000);
+        search.sendKeys("J");
+        Thread.sleep(2000);
+        search.sendKeys("a");
+        Thread.sleep(2000);
+        search.sendKeys("v");
+        Thread.sleep(2000);
+        search.sendKeys("a", Keys.ENTER);
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
