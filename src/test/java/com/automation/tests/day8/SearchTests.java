fe945b1bf0bddb602faf54bce58917990d4cae17
NeslihanAcan <neslihan3018@hotmail.com>
2020-03-14 21:02:39 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -18,23 +18,53 @@ public class SearchTests {
     private WebDriver driver;
 
     @Test
-    public void googleSearchTest(){
+    public void googleSearchTest() {
         driver.get("http://google.com");
         driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
         BrowserUtils.wait(2);
         //since every search item has a tag name <h3>
         //it's the easiest way to collect all of them
         List<WebElement> searchItems = driver.findElements(By.tagName("h3"));
-        for(WebElement searchItem: searchItems){
+        for (WebElement searchItem : searchItems) {
             String var = searchItem.getText();
             //if there is a text - print it
-            if(!var.isEmpty()){
-                System.out.println(var);
+            if (!var.isEmpty()) {
+                System.out.println(var.toLowerCase());
                 //verify that every search result contains java
                 Assert.assertTrue(var.toLowerCase().contains("java"));
             }
         }
     }
+
+    @Test(description = "Search for java book on amazon")
+    public void amazonSearchTest() {
+        driver.get("http://amazon.com");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java", Keys.ENTER);
+        BrowserUtils.wait(5);
+
+        List<WebElement> searchItems = driver.findElements(By.tagName("h2"));
+
+        searchItems.get(0).click();
+        BrowserUtils.wait(3);
+
+        WebElement productTitle = driver.findElement(By.id("ebooksProductTitle"));
+        String productTitleString = productTitle.getText();
+        System.out.println(productTitleString);
+
+        Assert.assertTrue(productTitleString.contains("Java"));
+    }
+
+    @Test
+    public void LogInTest(){
+
+        driver.get("http://practice.cybertekschool.com/");
+    }
+
+
+
+
 
     @BeforeMethod
     public void setup(){
